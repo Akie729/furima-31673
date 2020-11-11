807a0c69f6c3c56a138dcd811f8d21f2f2ef8688
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-11-11 17:06:53 +0900

    deviseのストロングパラメーターを設定

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 2020_11_10_102851) do
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "first_name_kana", null: false
+    t.string "first_name", null: false
     t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
