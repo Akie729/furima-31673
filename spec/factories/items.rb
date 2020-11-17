e48c96eb9d40da47ab6541ffbf5282e54b34359c
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-11-17 11:42:38 +0900

    itemモデル単体テストコードFactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name              { "花" }
+    description       { "きれいな花です" }
+    category_id       { "3" }
+    condition_id      { "2" }
+    delivary_fee_id   { "2" }
+    prefecture_id     { "2" }
+    days_to_ship_id   { "3" } 
+    price             { "500" }
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
