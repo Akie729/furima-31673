285330c7321a6f26e4531d7920cff3cff4f1a204
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-11-18 17:10:32 +0900

    before_actionで記述を簡潔化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, only: :edit
+  before_action :find_params_id, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -48,4 +46,9 @@ class ItemsController < ApplicationController
       redirect_to action: :index
     end
   end
+
+  def find_params_id
+    @item = Item.find(params[:id])
+  end
+
 end
