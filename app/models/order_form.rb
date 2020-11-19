33a80256d7614f6f97b509015270a864b0b9faf5
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-11-19 18:29:44 +0900

    購入ボタンのパス指定

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -13,6 +13,6 @@ class OderForm
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
-    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number,:building_name: building_name, phone: phone, order: order.id)
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone: phone, order: order.id)
   end
 end
