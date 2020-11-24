10e08ab30dd66aa47e2416c556bdb5eb5bc27eb0
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-11-24 15:18:45 +0900

    item_findメソッド作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,13 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: :index
   before_action :move_to_root_path, only: :index
   before_action :sold_out_item, only: [:index]
+  before_action :item_find, only: [:index, :create]
 
   def index
     @orderform = OrderForm.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @orderform = OrderForm.new(order_params)
     if @orderform.valid?
       pay_item
@@ -21,6 +20,10 @@ class OrdersController < ApplicationController
   end
 
   private
+
+  def item_find
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
     params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
@@ -36,8 +39,8 @@ class OrdersController < ApplicationController
   end
 
   def move_to_root_path
-    @order = Item.find(params[:item_id])
-    redirect_to root_path if user_signed_in? && current_user.id == @order.user_id
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if user_signed_in? && current_user.id == @item.user_id
   end
 
   def sold_out_item
