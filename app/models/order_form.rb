d02b61cd00477952ba88765b48f571e284a99bd9
Akie729 <72956349+Akie729@users.noreply.github.com>
2020-11-24 12:26:57 +0900

    orderformモデル単体テストコード実装

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -3,16 +3,15 @@ class OrderForm
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone, :user_id, :item_id, :token
   
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "はハイフンを含んでください"}
     validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
     validates :house_number
-    validates :phone, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "is invalid."}  
+    validates :phone, format: {with: /\A\d{10}\z|\A\d{11}\z/ , message: "は１１桁の数字を入力してください"}  
   end
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :token, presence: true
 
   def save
-    # binding.pry
     order = Order.create(user_id: user_id, item_id: item_id)
     Adress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone: phone, order_id: order.id)
   end
---
+++ spec/models/order_form_spec.rb
@@ -0,0 +1,65 @@
+require 'rails_helper'
+
+RSpec.describe OrderForm, type: :model do
+  before do
+    @orderform = FactoryBot.build(:order_form)
+  end
+
+  describe '商品購入機能' do
+    context '商品購入がうまくいくとき' do
+      it "郵便番号・都道府県・市区町村・番地・電話番号・tokenがあれば購入できる" do
+        expect(@orderform).to be_valid
+      end
+    end
+    
+    context '商品購入がうまくいかないとき' do
+      it "tokenが空では保存できないこと" do
+        @orderform.token = nil
+        @orderform.valid?
+        expect(@orderform.errors.full_messages).to include("Token can't be blank")
+      end
+
+      it "郵便番号が空だと保存できない" do
+        @orderform.postal_code = nil
+        @orderform.valid?
+        expect(@orderform.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it "郵便番号に-(ハイフン)が含まれていないと保存できない" do
+        @orderform.postal_code = "111111"
+        @orderform.valid?
+        expect(@orderform.errors.full_messages).to include("Postal code はハイフンを含んでください")
+      end
+
+      it "都道府県が１では保存できない" do
+        @orderform.prefecture_id= "1"
+        @orderform.valid?
+        expect(@orderform.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      
+      it "市町村がないと保存できない" do
+        @orderform.city = nil
+        @orderform.valid?
+        expect(@orderform.errors.full_messages).to include("City can't be blank")
+      end
+
+      it "番地がないと保存できない" do
+        @orderform.house_number = nil
+        @orderform.valid?
+        expect(@orderform.errors.full_messages).to include("House number can't be blank")
+      end
+
+      it "電話番号がないと保存できない" do
+        @orderform.phone = nil
+        @orderform.valid?
+        expect(@orderform.errors.full_messages).to include("Phone can't be blank")
+      end
+
+      it "電話番号は11桁ないと保存できない" do
+        @orderform.phone = "123456789"
+        @orderform.valid?
+        expect(@orderform.errors.full_messages).to include("Phone は１１桁の数字を入力してください")
+      end
+    end
+  end
+end
